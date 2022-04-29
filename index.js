886b6701921322d48086f28dc935f3eb20f1abae
Geicy Beatriz Soudré Cardoso <geicybeatriz@gmail.com>
2022-04-29 20:07:42 -0300

    feat: add sending userdata endpoint

--- index.js
+++ index.js
@@ -2,25 +2,42 @@ import express, { json } from "express";
 import chalk from "chalk";
 import cors from "cors";
 import { MongoClient} from "mongodb";
+import dotenv from "dotenv";
+import Joi from "joi";
+dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(json());
 
+//inicializando banco de dados
 let db = null;
-const mongoClient = new MongoClient("mongodb://localhost:27017");
-const calling = mongoClient.connect();
-calling.then(() => {
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+const promise = mongoClient.connect();
+promise.then(() => {
 	db = mongoClient.db("users_data");
 });
-calling.catch((erro) => console.log(erro));
+promise.catch((erro) => console.log(erro));
 
+//participantes
 app.post("/participants", (req, res) => {
+    const sendUser = {...req.body, lastStatus: Date.now()};
+    
+    //validação   
+    const participantsSchema = Joi.object({
+        name: Joi.string().min(1).required()
+    })
+    const validation = participantsSchema.validate(req.body);
+    if(validation.error){
+        res.sendStatus(422);
+    }
+
     //inserindo os usuarios
-    const promise = db.collection("participants").insertOne(req.body);
+    const promise = db.collection("participants").insertOne(sendUser);
     promise.then(() => {
         res.sendStatus(201);
     });
+    promise.catch((err) => console.log("Deu algum erro", err));
 });
 
 app.get("/participants", (req, res) => {
