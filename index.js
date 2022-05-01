1b67cf7a10ae105edb465358bff0f57e00e1c697
Geicy Beatriz Soudré Cardoso <geicybeatriz@gmail.com>
2022-05-01 20:15:30 -0300

    feat: add check user existence endpoint

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import chalk from "chalk";
 import cors from "cors";
 import { MongoClient} from "mongodb";
 import dotenv from "dotenv";
+import dayjs from "dayjs";
 import Joi from "joi";
 dotenv.config();
 
@@ -10,42 +11,72 @@ const app = express();
 app.use(cors());
 app.use(json());
 
-//inicializando banco de dados
+const participantsSchema = Joi.object({
+    name: Joi.string().min(1).required()
+});
+
 let db = null;
 const mongoClient = new MongoClient(process.env.MONGO_URI);
-const promise = mongoClient.connect();
-promise.then(() => {
-	db = mongoClient.db("users_data");
-});
-promise.catch((erro) => console.log(erro));
+mongoClient.connect(() => {
+    db = mongoClient.db("batepapo_uol_data");
+})
 
-//participantes
-app.post("/participants", (req, res) => {
-    const sendUser = {...req.body, lastStatus: Date.now()};
+app.post("/participants", async (req, res) => {
+    const user = req.body;
 
-    //validação   
-    const participantsSchema = Joi.object({
-        name: Joi.string().min(1).required()
-    })
-    const validation = participantsSchema.validate(req.body);
+    const validation = participantsSchema.validate(req.body, { abortEarly: false });
     if(validation.error){
         res.sendStatus(422);
         return;
     }
 
-    //inserindo os usuarios
-    const promise = db.collection("participants").insertOne(sendUser);
-    promise.then(() => {
+    try {
+        const namesList = await db.collection("participants").find({}).toArray();
+        console.log("nomes", namesList);
+        for(let i = 0; i < namesList.length; i++){
+            if(user.name === namesList[i].name){
+                res.status(409).send("O usuário ja existe");
+                return;
+            }
+        }
+        
+        await db.collection("participants").insertOne({name: user.name, lastStatus: Date.now()});
+        await db.collection("messages").insertOne({
+            from: user.name,
+            to: 'Todos',
+            text:'entra na sala...',
+            type: 'status',
+            time: dayjs().format("HH:mm:ss")
+        })
         res.sendStatus(201);
-    });
-    promise.catch((err) => console.log("Deu algum erro", err));
+        
+    } catch (error) {
+        console.log("Deu algum erro", error);
+        res.sendStatus(500);
+    }
 });
 
-app.get("/participants", (req, res) => {
-    //pegando os usuarios
-    const promise = db.collection("participants").find().toArray();
-    promise.then(participants => {res.send(participants);
-	});
+app.get("/participants", async (req, res) => {
+    try{
+        const participantsList =  await db.collection("participants").find().toArray();
+        res.send(participantsList);
+    } catch (error){
+        res.send("erro ao listar usuários");
+    }
 });
+
+//mensagens
+app.post("/messages", (req, res) => {
+    const {to, text, type} = req.body;
+    const {user} = req.headers;
+    res.send("ok");
+    
+})
+app.get("/messages", (req, res) => {
+    res.send("ok");
+    
+})
+
+
 
 app.listen(5000, () => console.log(chalk.bold.green("Servidor em pé na porta 5000")));
