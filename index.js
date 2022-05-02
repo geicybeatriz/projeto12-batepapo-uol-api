c3d3d298e652dee61aeba90ffcc32fee8291eb09
Geicy Beatriz Soudré Cardoso <geicybeatriz@gmail.com>
2022-05-02 00:43:17 -0300

    feat: add post messages endpoint

--- index.js
+++ index.js
@@ -15,6 +15,12 @@ const participantsSchema = Joi.object({
     name: Joi.string().min(1).required()
 });
 
+const messageSchema = Joi.object({
+    from: Joi.string().required(),
+    to: Joi.string().required(),
+    text:Joi.string().required()
+});
+
 let db = null;
 const mongoClient = new MongoClient(process.env.MONGO_URI);
 mongoClient.connect(() => {
@@ -32,7 +38,6 @@ app.post("/participants", async (req, res) => {
 
     try {
         const namesList = await db.collection("participants").find({}).toArray();
-        console.log("nomes", namesList);
         for(let i = 0; i < namesList.length; i++){
             if(user.name === namesList[i].name){
                 res.status(409).send("O usuário ja existe");
@@ -49,7 +54,6 @@ app.post("/participants", async (req, res) => {
             time: dayjs().format("HH:mm:ss")
         })
         res.sendStatus(201);
-        
     } catch (error) {
         console.log("Deu algum erro", error);
         res.sendStatus(500);
@@ -66,15 +70,61 @@ app.get("/participants", async (req, res) => {
 });
 
 //mensagens
-app.post("/messages", (req, res) => {
+app.post("/messages", async (req, res) => {
     const {to, text, type} = req.body;
     const {user} = req.headers;
-    res.send("ok");
+    const validMessage = {
+        from: user,
+        to:to,
+        text: text
+    };
+    //validação do corpo da mensagem com a lib joi:
+    const validation = messageSchema.validate(validMessage, { abortEarly: false });
+    if(validation.error){
+        res.sendStatus(422);
+        return;
+    }
     
+    //verificando tipo de mensagem, se é privada ou pública:
+    if(type !== "message" || type !== "private_message"){
+        res.sendStatus(422);
+        return;
+    }
+
+    try{
+        //verificando se o usuário que enviou a mensagem está na lista de participantes
+        const namesList = await db.collection("participants").find({}).toArray();
+        console.log("nomes", namesList);
+        for(let i = 0; i < namesList.length; i++){
+            if(user !== namesList[i].name){
+                res.status(422).send("O usuário não está mais na lista de participantes");
+                return;
+            }
+        }
+        //enviando mensagens para a coleção de mensagens
+        await db.collection("messages").insertOne({
+            from: user,
+            to: to,
+            text: text,
+            type: type,
+            time: dayjs().format("HH:mm:ss")
+        });
+        res.sendStatus(201);
+    } catch (error) {
+        console.log("Deu algum erro", error);
+        res.sendStatus(500);
+    }
 })
-app.get("/messages", (req, res) => {
-    res.send("ok");
-    
+
+//enviando mensagens para o front-end 
+app.get("/messages", async (req, res) => {
+    try{
+        const messagesList =  await db.collection("messages").find().toArray();
+        res.send(messagesList);
+    } catch (error){
+        console.log("error", error);
+        res.send("erro ao listar as mensagens");
+    }
 })
 
 
